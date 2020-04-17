58e0a2ab1e6c53ea5ae8167a310f94a6b59ffabc
Bayo Ade <bayo.300@gmail.com>
2020-04-17 11:26:13 +0100

    Going over React basics again for a new perspective

---
+++ src/components/card-list/card-list-component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Card } from '../card/card-component'
+
+// Style
+import './card-list-styles.css'
+
+const CardList = (props) => {
+
+    return (
+        <div className='card-list'>
+            {props.monsters.map(function(monster){
+            return (
+            <Card key={monster.id} monster={monster} />
+            )
+          })}
+        </div>
+    )
+}
+
+export default CardList
